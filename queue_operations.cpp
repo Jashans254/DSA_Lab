5194d8f4fc1561d21f072ec0ad593db61223b9a7
jashanpreet singh <jashanprts254@gmail.com>
2023-10-07 11:46:00 +0530

    added queue ds

---
+++ queue_operations.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+using namespace std;
+int a[5],n = 5 ;
+int front = -1 ,rear = -1;
+void enqueue(int item)
+{
+    if(rear == n-1)
+    {
+        cout<<"\nOverflow occured..\n";
+        return;
+    }
+    if(front == -1)
+    {
+        front = 0;
+    }
+    a[++rear] = item;
+}
+void dequeue()
+{
+    if(rear == -1)
+    {
+        cout<<"\nUnderflow occured\n";
+    }
+    else{
+        cout<<"\nDeleted element is "<<a[front];
+        if(front == rear)
+        {
+        front = rear = -1;
+        }
+        else{
+        front++;
+        }
+    } 
+}
+void display()
+{
+    if(rear == -1)
+    {
+        cout<<"\nEmpty queue";
+    }
+    else{
+        for(int i = front ; i<=rear; i ++)
+    {
+        cout<<a[i]<<" ";
+    }
+    }
+   
+}
+int main() 
+{
+    int ch;
+    cout<<"\n1 - To insert new element\n";
+    cout<<"\n2 - To delete existing element\n";
+    cout<<"\n3 - To display all the elements\n";
+    cout<<"\n4- To exit\n";
+    do{
+    cout<<"\nEnter your choice:";
+    cin>>ch;
+    switch(ch){
+        case 1:
+            cout<<"\nEnter the value of element:";
+            int value;
+            cin>>value;
+            enqueue(value);
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            cout<<"\nexiting";
+            break;
+        default:
+            cout<<"\nInvalid choice";
+            break;
+    }
+    }while(ch!=4);
+    
+    return 0;
+}
